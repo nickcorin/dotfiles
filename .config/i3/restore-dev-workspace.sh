c07987bd88ba13f30c6d99e03211a3f78db346d5
Nick Corin <nickcorin@gmail.com>
2020-04-11 12:54:04 +0000

    i3: Patch picom race condition; Fix alacritty tmux command

--- .config/i3/restore-dev-workspace.sh
+++ .config/i3/restore-dev-workspace.sh
@@ -4,5 +4,5 @@
 i3-msg "workspace 2:dev; append_layout ~/.config/i3/dev-workspace.json"
 
 # Autostart applications.
-(alacritty --class nvim --working-directory /home/nick/go -e tmux &)
-(alacritty --class term --working-directory ~ -e tmux &)
+(alacritty --class nvim --working-directory /home/nick/go -e tmux -f ~/.tmux.conf &)
+(alacritty --class term --working-directory ~ -e tmux -f ~/.tmux.conf &)
