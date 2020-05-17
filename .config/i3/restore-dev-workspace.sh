6e7844bcd81f1867b32a99fca4eefc10f4ce1d2c
Nick Corin <nickcorin@gmail.com>
2020-05-17 10:42:24 +0000

    i3: Re-work development workspace

--- .config/i3/restore-dev-workspace.sh
+++ .config/i3/restore-dev-workspace.sh
@@ -6,3 +6,4 @@ i3-msg "workspace 2:dev; append_layout ~/.config/i3/dev-workspace.json"
 # Autostart applications.
 (kitty --name nvim -d /home/nick/go tmux &)
 (kitty --name term -d ~ tmux &)
+(kitty --name term -d ~ tmux &)
