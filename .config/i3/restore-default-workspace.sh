cf2399bd023e2c01da6579640d382d58dc1fdecb
Nick Corin <nickcorin@gmail.com>
2020-05-23 10:35:14 +0000

    i3: Replace kitty with alacritty

--- .config/i3/restore-default-workspace.sh
+++ .config/i3/restore-default-workspace.sh
@@ -4,4 +4,4 @@
 i3-msg "workspace 1:default; append_layout ~/.config/i3/default-workspace.json"
 
 # Autostart applications.
-(kitty --hold --name neofetch neofetch &)
+(alacritty --hold --class neofetch -e neofetch &)
--- .config/i3/restore-dev-workspace.sh
+++ .config/i3/restore-dev-workspace.sh
@@ -4,6 +4,7 @@
 i3-msg "workspace 2:dev; append_layout ~/.config/i3/dev-workspace.json"
 
 # Autostart applications.
-(kitty --name nvim -d /home/nick/go tmux &)
-(kitty --name term -d ~ tmux &)
-(kitty --name term -d ~ tmux &)
+(alacritty --class nvim --working-directory /home/nick/go -e tmux &)
+(alacritty --class term --working-directory ~ -e tmux &)
+(alacritty --class term --working-directory ~ -e tmux &)
+
