2af4f09b83f44206b72ef36c0935b55c125308a7
Nick Corin <nickcorin@gmail.com>
2020-11-06 16:13:32 +0200

    nvim: Work on install script

--- .config/nvim/install.sh
+++ .config/nvim/install.sh
@@ -1,77 +1,145 @@
 #!/bin/bash
+#
+# An all-in-one install script for my personal development environment. It is
+# somewhat OS agnostic but not entirely - it does a quick check for Darwin vs
+# Linux but not for any specific distributions for now (Ubuntu-based distros
+# are assumed).
+
+# The list of programs to install.
+dependencies=(
+	"git"
+	"nvim"
+	"ripgrep"
+	"tmux"
+)
 
 # Default dotfiles directory.
 DOTFILES_DIR=${DOTFILES_DIR-"$HOME/dotfiles"}
 
+###############################################################################
+# Installs the missing dependencies.
+# Globals:
+# 	dependencies
+# Arguments:
+# 	None
+###############################################################################
+install_dependencies() {
+	missing_deps=()
+	for dep in "${dependencies[@]}"; do
+		if ! is_installed "$dep"; then
+			missing_deps+=("$dep")
+		fi
+	done
+
+	if [[ "${#missing_deps[@]}" -eq 0 ]]; then
+		printf "It looks like you've already got everything installed.\n"
+	else
+		printf "\nI've detected ${#missing_deps[@]} missing dependencies.\n"
+		read -p "Would you like me to install them now? (Y/n): " answer
+		case $answer in
+			[Nn]* )
+				printf "\nOkay, we don't have to do it now.\n"
+				printf "Let me know when you're ready. :)\n"
+				exit 0
+				;;
+			*)
+				printf "Okay, let's do it.\n"
+				for dep in "${missing_deps[@]}"; do
+					install "$dep"
+				done
+				;;
+		esac
+	fi
+}
+
+###############################################################################
+# Clone the Git repository containing the necessary configuration files for the
+# various programs we will install.
+# Globals:
+#   DOTFILES_DIR
+# Arguments:
+#   None
+###############################################################################
 clone_dotfiles() { 
-	if [[ ! -d "$DOTFILES_DIR" ]]
-	[ ! -d $HOME/dotfiles ] || 
-		\ printf "\"$HOME/dotfiles\" already exists. Remove it and try again." &&
-		\ exit 1;
-
-	git clone https://github.com/nickcorin/dotfiles.git $HOME/dotfiles
+	if [[ -d "$DOTFILES_DIR" ]]; then
+	  err "\"$HOME/dotfiles\" already exists. Remove it and try again."
+	  exit 1
+	else
+	  git clone https://github.com/nickcorin/dotfiles.git $HOME/dotfiles
+	fi
 }
 
-install_brew() { 
-	printf "Installing brew.."
+###############################################################################
+# Print an error string to stderr.
+# Globals:
+# 	None
+# Arguments:
+# 	The error string.
+###############################################################################
+err() {
+	echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
 }
 
-install_neovim() { 
-	printf "Installing neovim.."
+###############################################################################
+# Check whether a program is installed.
+# Globals:
+# 	None
+# Arguments:
+#		Program to check, a command.
+# Returns:
+# 	0 if the program is installed, 1 if it is not.
+###############################################################################
+is_installed() {
+	printf "checking if $1 is installed.."
+	if ! command -v "$1" &> /dev/null; then
+		printf "no\n"
+		false
+	else
+		printf "yes\n"
+		true
+	fi
 }
 
-install_pip() { 
-	printf "Installing pip3.."
-}
-
-install_pynvim() {
-	printf "Installing pynvim.."
-}
-
-install_node() {
-	printf "Installing node.."
-}
-
-function install() {
+###############################################################################
+# Install a program using the default package manager.
+# Globals:
+# 	None
+# Arguments:
+# 	Program to install.
+###############################################################################
+install() {
+	printf "Installing $1.."
 	shopt -s nocasematch
 	case "$(uname -s)" in
-		darwin) install_darwin $1 ;;
-		linux) 	install_linux  $1 ;;
-		*) 		printf "No idea what OS you're on.. exiting."; exit 1
+		linux)
+			install_linux  $1
+			;;
+		*)
+			err "I'm sorry, I can only work with Ubuntu-base linux distributions"
+			err "I apologize for the inconvenience."
+			exit 1
+			;;
 	esac
 }
 
-function install_darwin() {
-	brew install $1
+###############################################################################
+# Install a program using apt.
+# Globals:
+# 	None
+# Arguments:
+# 	Program to install.
+###############################################################################
+install_linux() {
+	sudo apt install -y $1
 }
 
-function install_linux() {
-	sudo apt install $1
-}
+printf "Hi there, it's good to have you back.\n"
+printf "Let's get your development setup ready for you.\n"
+printf "\n"
 
-
-printf "Installing NVIM++"
-
-if [[ "$(uname)" == "Darwin" ]] ; then
-	if [[ $(which brew) > /dev/null ]] ; then
-		printf "brew is installed, moving on.."
-	fi
+if ! install_dependencies; then
+	exit 1
 fi
 
-# Check that Neovim is installed.
-which nvim > /dev/null && printf "neovim is installed, moving on.." ||
-	\ install_neovim()
-
-# Check that pip3 is installed.
-which pip3 > /dev/null && printf "pip3 is installed, moving on.." ||
-	\ install_neovim()
-
-# Check that node is installed.
-which node > /dev/null && printf "node is installed, moving on.." ||
-	\ install_node()
-
-# Check that pynvim is installed.
-pip3 list | grep pynvim > /dev/null && printf "pynvim is installed, moving on.."
-	\ || install_pynvim()
-
-
+printf "\nIt looks like we're all done here.\n"
+printf "Happy hacking!\n"
