37389af4946764649e3e8b1f82dba8f84c91cffd
Nick Corin <nickcorin@gmail.com>
2022-02-02 09:53:40 +0200

    fish: Add fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,10 @@
+# Set default git credentials.
+if command -v git > /dev/null
+        git config --global user.name "Nick Corin"
+        git config --global user.email "nickcorin@gmail.com"
+end
 
-abbr -a please "sudo"
-abbr -a code "open -a ~/applications/visual\ studio\ code.app"
-
+# Use exa if it's available, but keep usage consistent either way.
 if command -v exa > /dev/null
 	abbr -a l 'exa'
 	abbr -a ls 'exa'
@@ -13,17 +16,20 @@ else
 	abbr -a lll 'ls -la'
 end
 
-# Environment variables.
-set -x  EDITOR "vim"
-set -x  GO111MODULE on
-set -x  GOPATH "$HOME/dev/go"
-set -x  PATH "$PATH:$GOPATH/bin"
-set -x  PATH "$PATH:$HOME/.cargo/bin"
-set -x  XDG_CONFIG_HOME "$HOME/.config"
-
 # You may need to manually set your language environment
- set -x  LANG en_US.UTF-8
- set -x  LC_ALL en_US.UTF-8
+set -g -x LANG en_US.UTF-8
+set -g -x LC_ALL en_US.UTF-8
+
+# Environment variables
+set -g -x EDITOR "nvim"
+set -g -x GO111MODULE on
+set -g -x GOPATH "$HOME/dev/go"
+set -g -x XDG_CONFIG_HOME "$HOME/.config"
+
+# Setting up PATH
+set -g -x PATH "$PATH:/opt/homebrew/bin"
+set -g -x PATH "$PATH:$GOPATH/bin"
+set -g -x PATH "$PATH:$HOME/.cargo/bin"
 
 # Fish git prompt
 set __fish_git_prompt_showuntrackedfiles 'yes'
@@ -32,7 +38,6 @@ set __fish_git_prompt_showstashstate ''
 set __fish_git_prompt_showupstream 'none'
 set -g fish_prompt_pwd_dir_length 3
 
-# Customise prompt.
 function fish_prompt
 	set_color brblack
 	echo -n "["(date "+%H:%M")"] "
@@ -42,7 +47,7 @@ function fish_prompt
 		set_color brblack
 		echo -n ':'
 		set_color yellow
-		printf '%s ' (prompt_pwd)
+		echo -n (basename $PWD)
 	end
 	set_color green
 	printf '%s ' (__fish_git_prompt)
@@ -51,9 +56,25 @@ function fish_prompt
 	set_color normal
 end
 
-# Check if there is a private zshrc to souce. Private zshrc files contain
-# environment specific variables and functions that shouldn't be added to
-# Git.
-if test -e ~/.private.fish
-	source ~/.private.fish
+function fish_greeting
+end
+
+# Start tmux when the shell opens.
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	# builtin source ~/.config/fish/conf.d/base16.fish
+	
+	set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+
+	# Always start tmux on start, but attach to the old session if it's still active.
+	if ! set -q TMUX
+		exec tmux new-session -As (hostname -s)
+	end
+end
+
+# Exit tmux when the shell closes to prevent infinite tmux sessions from being started.
+function on_exit --on-process-exit $fish_pid
+	exec echo $fish_pid > ~/exited_session.txt
+	exec tmux kill-session -t $fish_pid
 end
