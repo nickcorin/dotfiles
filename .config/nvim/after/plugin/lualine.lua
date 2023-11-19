d450644741fac297fa3530c8c26e1b8df0e9d65b
Nick Corin <nickcorin@gmail.com>
2023-11-19 12:40:09 +0200

    nvim: Update lualine colorscheme to everforest

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require("lualine").setup({
     options = {
         icons_enabled = true,
-        theme = 'rose-pine',
+        theme = 'everforest',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
