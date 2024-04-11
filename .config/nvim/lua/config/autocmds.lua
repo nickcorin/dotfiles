68f55ba2a86543f14e38fc163af30c5b5c3d8c02
Nick Corin <nickcorin@gmail.com>
2024-04-11 10:39:08 +0200

    nvim: Add useful autocmds; Fix formatting in some files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,10 +1,30 @@
 local api = vim.api
 
+-- Don't auto comment new line.
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-- Format on save.
+api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        vim.lsp.buf.format()
+    end
+})
+
+api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+    end
+})
+
 -- Highlight on yank.
 api.nvim_create_autocmd("TextYankPost", {
     pattern = "*",
     callback = function()
-       vim.highlight.on_yank({higroup = 'IncSearch', timeout = 1000})
+        vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 1000 })
     end
 })
 
+-- Resize neovim split when terminal is resized.
+vim.api.nvim_command("autocmd VimResized * wincmd =")
