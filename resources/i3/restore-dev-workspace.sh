aca8fe99149c09d0922fe7703101f411e7c3ed57
Nicholas Corin <nickcorin@gmail.com>
2025-07-09 13:18:41 +0200

    refactor: replace hardcoded paths with environment variables
    Replaced hardcoded username paths with appropriate environment variables:
    - Use $HOME instead of /Users/nick or /home/nick
    - Use $GOPATH for Go workspace directory
    - Use $(chezmoi source-path) for dotfiles paths
    
    This improves privacy and makes dotfiles more portable across systems.

--- resources/i3/restore-dev-workspace.sh
+++ resources/i3/restore-dev-workspace.sh
@@ -4,7 +4,7 @@
 i3-msg "workspace 2:dev; append_layout ~/.config/i3/dev-workspace.json"
 
 # Autostart applications.
-(alacritty --class nvim --working-directory /home/nick/go -e tmux &)
+(alacritty --class nvim --working-directory "$GOPATH" -e tmux &)
 (alacritty --class term --working-directory ~ -e tmux &)
 (alacritty --class term --working-directory ~ -e tmux &)
 
