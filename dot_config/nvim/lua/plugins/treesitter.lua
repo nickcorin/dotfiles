68788bea1d5be5f41f0b0b9dabb4c5d500df1138
Nicholas Corin <nickcorin@gmail.com>
2025-07-10 15:13:24 +0200

    config: remove nvim lazy-lock.json and update various configurations
    Remove Neovim's lazy-lock.json file which locks plugin versions.
    Update chezmoi configuration, ghostty settings, treesitter plugin
    configuration, and zsh environment. Add new packages to chezmoidata.
    
    The removal of lazy-lock.json allows plugins to update to their
    latest versions rather than being locked to specific commits.

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    build = function()
-        vim.cmd("TSUpdate")
-    end,
-    opts = {
-        ensure_installed = "all",
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-        },
-        textobjects = {
-            select = {
-                enable = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                },
-            },
-        },
-    },
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	lazy = false,
+	opts = {
+		auto_install = true,
+		ensure_installed = "all",
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+		ignore_install = { "ipkg" },
+	},
 }
